766e615ae1db740f981ca3fade4c44fb77264c5d
Aayan <slmngoyo2004@gmail.com>
2024-07-31 23:53:35 +0300

    some changes

--- routes/api.js
+++ routes/api.js
@@ -109,8 +109,8 @@ router.get('/users/:_id/logs', async (req, res) => {
               return res.status(400).send('Invalid query parameters');
            } else {
                logs = await Exercise.find({id: id, date: {$gte: new ISODate(fromDate), $lte: new ISODate(toDate)}})
-                                    .lean()
                                     .limit(logLimit)
+                                    .lean()
                                                  
               
            }
