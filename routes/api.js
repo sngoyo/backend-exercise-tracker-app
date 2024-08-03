02707004f82b9e392b992a512ec2e2ed26e9a36a
Aayan <slmngoyo2004@gmail.com>
2024-08-04 01:38:00 +0300

    some changes

--- routes/api.js
+++ routes/api.js
@@ -114,7 +114,7 @@ router.get('/users/:_id/logs', async (req, res) => {
            return {
             'description': log.description, 
             'duration': log.duration, 
-            'date': new Date(log.date).toDateString()
+            'date': (new Date(log.date)).toDateString()
          }
         });
       
