9808b2c6487767d41c724bea7069967bd4bed0f8
Aayan <slmngoyo2004@gmail.com>
2024-08-04 15:19:39 +0000

    some changes

--- routes/api.js
+++ routes/api.js
@@ -139,11 +139,11 @@ router.get('/users/:_id/logs', async (req, res) => {
 
         
         //Changing date format value in retrieved logs from database  from the mongodb date format to dateString
-          logs = logs.map(log => ({
+         /* logs = logs.map(log => ({
             description: log.description, 
             duration: log.duration, 
             date: new Date(log.date).toDateString()
-         }))
+         }))*/
 
          let newLogList = [];
 
