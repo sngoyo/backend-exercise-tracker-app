e8eba98462ce708e0170c7504866d4d635e28921
Aayan <slmngoyo2004@gmail.com>
2024-08-04 11:40:05 +0000

    some changes

--- models/usersModel.js
+++ models/usersModel.js
@@ -4,8 +4,10 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+        
     }
-})
-
+    
+},  { versionKey: false }
+)
 
 module.exports = mongoose.model('User', userSchema);
